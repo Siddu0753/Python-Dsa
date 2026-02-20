057a59d56db061f4ece18f6d4e4ab1f6e52d2849
siddu <siddu14x@gmail.com>
2026-02-21 05:13:34 +0530

    Store Frequency in Dictionary

---
+++ frequencymapDict.py
@@ -0,0 +1,35 @@
+
+
+# def mapping(list):
+#     dict1=dict()
+#     for i in range (0,len(list)):
+#         if list[i] in dict:
+#             dict1[list[i]]+=1
+#         else:
+#             dict1[list[i]]=1
+            
+#     return dict1
+
+# print(mapping([1,2,4,3,2,5,6,5,7,4,3,2,1]))
+
+# tc:O(n)
+# sc:O(n)>worst case
+
+
+
+#====================================================================
+
+# def mapping(list):
+#     dict1=dict()
+#     for i in range (0,len(list)):
+#         dict1[list[i]]=dict1.get(list[i],0)+1
+#     print(dict1[4])
+#     return dict1
+# print(mapping([1,2,1,2,1,2,3,4,5,4,4,35,5]))
+
+# tc:O(n)
+# sc:O(1)
+            
+            
+            
+    
