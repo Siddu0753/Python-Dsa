0deaab25e6917fb4542c68b03dfb853491aa493c
Siddu0753 <siddu14x@gmail.com>
2026-07-02 01:06:29 +0530

    added an optimized solution

--- problems/6.rytRotateArrByKPlaces.py
+++ problems/6.rytRotateArrByKPlaces.py
@@ -3,7 +3,8 @@ nums=[5,-2,3,9,0,6,10,7]
 def rot(nums):
     n=len(nums)
     k=3
-    for i in range(0,k):
+    rotations=k%n
+    for i in range(0,rotations):
         temp=nums[n-1]
         for j in range(n-2,-1,-1):
             nums[j+1]=nums[j]
