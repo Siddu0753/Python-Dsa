91aa1ef224b45dff67e18a23a6c2afb2f62c9add
Siddu0753 <siddu14x@gmail.com>
2026-07-01 21:33:23 +0530

    rotate an array by K elements

---
+++ problems/6.rytRotateArrByKPlaces.py
@@ -0,0 +1,13 @@
+
+nums=[5,-2,3,9,0,6,10,7]
+def rot(nums):
+    n=len(nums)
+    k=3
+    for i in range(0,k):
+        temp=nums[n-1]
+        for j in range(n-2,-1,-1):
+            nums[j+1]=nums[j]
+        nums[0]=temp
+    return nums
+
+print(rot(nums))
