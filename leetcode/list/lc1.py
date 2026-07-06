5ea679c3aa50988aab993e1411ef1f572f73a6af
Siddu0753 <siddu14x@gmail.com>
2026-07-07 00:54:12 +0530

    Two Sum Problem

---
+++ leetcode/list/lc1.py
@@ -0,0 +1,35 @@
+
+# nums = [2,7,11,15], target = 9
+# def twoSum(nums, target):
+#         n=len(nums)
+#         result=[]
+#         for i in range(0,n):
+#             for j in range(i+1,n):
+#                 if target==nums[i]+nums[j]:
+#                     result.append(i)
+#                     result.append(j)
+
+
+#         return result
+# print(twoSum(nums, target))
+# # tc:O(n^2),sc:O(1)
+
+
+############################################################
+print("optimized solution")
+nums = [2,7,11,15]
+target = 9
+def twoSum(nums, target):
+      n=len(nums)
+      result={}
+      need=0
+      final=[]
+      for i in range(0,n):  
+            need=target-nums[i]
+            if need not in result:
+                  result[nums[i]]=i
+            else:
+                  return [result[need],i]
+                
+       
+print(twoSum(nums, target))
