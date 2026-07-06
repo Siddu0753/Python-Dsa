a4348507678f5898e9308c7dd5b3c937b23a74d2
Siddu0753 <siddu14x@gmail.com>
2026-07-06 23:25:53 +0530

    find max consecutiveonesin a list

---
+++ leetcode/list/lc485.py
@@ -0,0 +1,15 @@
+nums= [1,1,0,1,1,1]
+
+def findMaxConsecutiveOnes(nums):
+    n=len(nums)
+    count=0
+    maxcount=0
+    for i in range(0,n):
+        if nums[i]==1:
+            count+=1
+            maxcount=max(count,maxcount)
+        else:
+            count=0
+    return maxcount
+
+print(findMaxConsecutiveOnes(nums))
