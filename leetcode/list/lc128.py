4ada788583fdd321822f8d419a3af8e2ee401b87
Siddu0753 <siddu14x@gmail.com>
2026-07-09 20:55:36 +0530

    longest consecutive sequence

--- leetcode/list/lc128.py
+++ leetcode/list/lc128.py
@@ -1,8 +1,31 @@
 
-nums = [100,4,200,1,3,2]
+nums = [0]
 
-n=len(nums)
-cl=0
-fl=0
-for i in range(0,n):
+ml=0
+for num in nums:
     
+    if num - 1 not in set(nums):
+        l=1
+        
+        while num+l in set(nums):
+            l+=1
+        if l>ml:
+            ml=l
+    l=0
+
+print(ml)
+
+
+        
+        
+
+    
+
+        
+
+
+
+
+
+
+        
