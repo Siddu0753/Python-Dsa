4aa5d07103c8d3c1c105b3cefe292e2b5121672e
Siddu0753 <siddu14x@gmail.com>
2026-07-12 17:58:21 +0530

    spiral matrix

---
+++ 5.2D List or Matrix/2dlist/lc54.py
@@ -0,0 +1,34 @@
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+r=len(matrix)-1
+l=0
+t=0
+b=len(matrix)-1
+result=[]
+
+while t<=b and l<=r:
+    for i in range(l,r+1):
+        result.append(matrix[t][i])
+    t+=1
+    for i in range(t,b+1):
+        result.append(matrix[i][r])
+    r-=1
+    for i in range(r,l-1,-1):
+        result.append(matrix[b][i])
+    b-=1
+    for i in range(b,t-1,-1):
+        result.append(matrix[i][l])
+    l+=1
+
+print(result)
+
+
+
+
+      
+
+
+        
+         
+        
+        
