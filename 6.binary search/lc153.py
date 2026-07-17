2ff92894c3f7360d7645367e76943cca598dc656
Siddu0753 <siddu14x@gmail.com>
2026-07-17 17:59:36 +0530

    Find Minimum in Rotated Sorted Array 100% beat

---
+++ 6.binary search/lc153.py
@@ -0,0 +1,26 @@
+# . Find Minimum in Rotated Sorted Array
+# Medium
+
+nums = [3,4,5,1,2]
+
+def fun(nums):
+    n=len(nums)
+    low=0
+    high=n-1
+    while low<high:
+        mid=(low+high)//2
+
+        if nums[mid]>nums[high]:
+            low=mid+1
+        else:
+            high=mid
+    return nums[mid]
+print(fun(nums))
+
+
+
+        
+
+
+
+
